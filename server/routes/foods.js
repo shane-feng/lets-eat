456fef2f12a2bc9a7211d5c250e344167ed15539
Shane F <shanevi29@gmail.com>
2021-09-16 21:52:26 -0700

    update fetch food items endpoint to fetch foodsitems to be eaten today

--- server/routes/foods.js
+++ server/routes/foods.js
@@ -13,12 +13,34 @@ router.post('/', async (req, res) => {
   }
 });
 
+// fetch food items
 router.get('/', async (req, res) => {
-  try {
-    const results = await Food.find({});
-    res.send(results);
-  } catch (e) {
-    res.status(500).send();
+  // fetch food items that should be eaten today
+  if (req.query.date) {
+    const today = new Date();
+
+    try {
+      const results = await Food.find();
+      filteredResults = results.filter(({ dateToEat }) => {
+        return (
+          dateToEat?.getFullYear() == today.getFullYear() &&
+          dateToEat?.getMonth() == today.getMonth() &&
+          dateToEat?.getDate() + 1 == today.getDate()
+        );
+      });
+      res.send(filteredResults);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send();
+    }
+  } else {
+    // fetch all food items
+    try {
+      const results = await Food.find();
+      res.send(results);
+    } catch (e) {
+      res.status(500).send();
+    }
   }
 });
 
