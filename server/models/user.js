3ee40b6d9ab16079e4a3bed1e1feeba387e46752
Shane F <shanevi29@gmail.com>
2021-09-20 11:53:54 -0700

    add static method to User model to find user by creentials

--- server/models/user.js
+++ server/models/user.js
@@ -38,6 +38,22 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+// create static method on User model to find user by credentials
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+  return user;
+};
+
 // middleware to hash user password prior to saving to mongodb
 userSchema.pre('save', async function (next) {
   const user = this;
