ca48b797ebc47faf58c38a9148c1a7ad9d565877
Shane F <shanevi29@gmail.com>
2021-09-21 14:28:49 -0700

    create config directory and file

---
+++ server/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  port: process.env.PORT || 8000,
+  dbURL: process.env.DATABASE_URL,
+  secret: process.env.SECRET || 'secret',
+  tokenExpiresInHours: process.env.TOKEN_EXPIRES_IN_HOURS || 24,
+};
