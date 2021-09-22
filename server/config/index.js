f2f20cfaf70abc2108c659b458d08264ad50ebf1
Shane F <shanevi29@gmail.com>
2021-09-21 17:51:41 -0700

    fix token expies fallback time

--- server/config/index.js
+++ server/config/index.js
@@ -2,5 +2,5 @@ module.exports = {
   port: process.env.PORT || 8000,
   dbURL: process.env.DATABASE_URL,
   secret: process.env.SECRET || 'secret',
-  tokenExpiresInHours: process.env.TOKEN_EXPIRES_IN_HOURS || 24,
+  tokenExpiresInHours: process.env.TOKEN_EXPIRES_IN_HOURS || '24h',
 };
