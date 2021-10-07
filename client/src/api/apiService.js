e1ad41a24da9a4729f9ef90effade92d55fbcf9d
Shane F <shanevi29@gmail.com>
2021-10-07 00:36:19 -0700

    add user logout api request

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -1,10 +1,18 @@
-const axios = require('axios');
-const { SERVER_URL } = require('../config');
+import axios from 'axios';
+import { SERVER_URL } from '../config';
+import { getSessionData } from '../utils';
 
 const apiService = axios.create({ baseURL: SERVER_URL });
 
 export const signupUser = async (userInfo) => {
   const queryPath = '/users/signup';
   const res = await apiService.post(queryPath, userInfo);
+  return res;
+};
+
+export const logoutUser = async () => {
+  const { session } = getSessionData();
+  const queryPath = '/users/logout';
+  const res = await apiService.post(queryPath, {}, { headers: { Authorization: `Bearer ${session.token}` } });
   return res;
 };
