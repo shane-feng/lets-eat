eaaf7174f926493df7f2cdd60b9aecf518e7b370
Shane Feng <shanevi29@gmail.com>
2022-01-14 00:09:43 -0800

    deploy v2 client

--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 8000,
-  dbURL: process.env.DATABASE_URL,
-  secret: process.env.SECRET || 'secret',
-  tokenExpiresInHours: process.env.TOKEN_EXPIRES_IN_HOURS || '24h',
-  requestSizeLimit: process.env.REQUEST_SIZE_LIMIT || '5mb',
-};
